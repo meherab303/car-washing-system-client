596c0c36dc5aa0f33097c63f1a94544cbae0684f
meherab nahin <nahinrahman87@gmail.com>
2025-03-08 17:29:52 +0600

    add error page in home

---
+++ src/app/(userCommonLayaout)/(home)/@recentServices/error.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+"use client";
+
+import { Card, CardBody } from "@heroui/card";
+import { Button } from "@heroui/react";
+
+
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Card className="w-full max-w-md p-6 text-center shadow-lg">
+        <CardBody>
+          <h2 className="text-2xl font-semibold text-red-600">Oops! Something went wrong</h2>
+          <p className="mt-2 text-gray-600">{error.message}</p>
+          <Button 
+            className="mt-4 text-white bg-red-500 hover:bg-red-600 transition" 
+            onClick={reset}
+          >
+            Try Again
+          </Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
--- src/app/(userCommonLayaout)/(home)/@recentServices/page.tsx
+++ src/app/(userCommonLayaout)/(home)/@recentServices/page.tsx
@@ -1,36 +1,3 @@
-// /* eslint-disable prettier/prettier */
-
-// import { Button } from "@heroui/react";
-// import Link from "next/link";
-
-// import Container from "@/src/components/modules/UI/container";
-// import getCarService from "@/src/services/carServices";
-// import CarServiceCard from "@/src/components/carServiceCard";
-// import { TService } from "@/src/types/carServiceTypes";
-
-
-// export default async function RecentServices() {
-//   const {data:services}=await getCarService()
-
-//   return (
-//     <Container>
-    //   <div className="section-title my-8">
-    //     <h2 className="mb-2 text-center text-2xl">Recent Services</h2>
-    //     <p className="text-center">
-    //       A list of items that have been recently found and reported.
-    //     </p>
-    //   </div>
-//       <div className="my-8 grid justify-center gap-10 sm:grid-cols-1 md:grid-cols-3">
-    
-//       {services.slice(0,3).map((service:TService) => (
-//         <CarServiceCard key={service._id} service={service} />
-//     ))}
-//       </div>
-    
-//     </Container>
-//   );
-// }
-
 /* eslint-disable prettier/prettier */
 
 import CarServiceCard from "@/src/components/carServiceCard";
@@ -44,7 +11,9 @@ import { TService } from "@/src/types/carServiceTypes";
 export default async function Servicesss() {
     const {data:services}=await getCarService()
 
- 
+  
+
+   
 
   return (
     <Container >
@@ -59,7 +28,9 @@ export default async function Servicesss() {
         <CarServiceCard key={service._id} service={service} />
     ))}
         </div>
-        <SeeAll/>
+       <div className=" mb-5">
+       <SeeAll/>
+       </div>
       
     </Container>
   );
