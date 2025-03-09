53cf3942f3b98b7c636a7d82dd6f2b333891e050
meherab nahin <nahinrahman87@gmail.com>
2025-03-09 17:52:07 +0600

    service details page

--- src/components/carServiceCard.tsx
+++ src/components/carServiceCard.tsx
@@ -1,30 +1,40 @@
 /* eslint-disable prettier/prettier */
-
 "use client";
 
- import {Card, CardHeader, CardBody, Image, CardFooter, Button} from "@heroui/react";
+
+
+import { useRouter } from "next/navigation";
+import { Card, CardHeader, CardBody, Image, CardFooter, Button } from "@heroui/react";
 
 import { TService } from "../types/carServiceTypes";
 
-const CarServiceCard = ({service}:{service:TService}) => {
+
+
+const CarServiceCard = ({ service }: { service: TService }) => {
+  const { _id, name, description, price } = service;
+  const router = useRouter();
+
   return (
-    <Card className="w-full max-w-[320px] p-3 shadow-lg hover:shadow-xl transition-shadow">
+    <Card
+      className="w-full max-w-[320px] p-3 shadow-lg hover:shadow-xl transition-shadow cursor-pointer"
+     
+    >
       <CardHeader className="pb-0">
-        <h2 className="text-xl font-semibold">{service.name}</h2>
+        <h2 className="text-xl font-semibold">{name}</h2>
       </CardHeader>
 
       <CardBody>
         <Image
-          alt={service.name}
+          alt={name}
           className="w-full h-[180px] object-cover rounded-lg"
           src="https://heroui.com/images/hero-card-complete.jpeg"
         />
-        <p className="text-gray-600 mt-2">{service.description}</p>
+        <p className="text-gray-600 mt-2">{description}</p>
       </CardBody>
 
       <CardFooter className="flex justify-between items-center">
-        <p className="font-bold text-blue-500">${service.price}</p>
-        <Button className="bg-transparent hover:bg-slate-800 hover:text-white hover:transition-all hover:duration-1000 ease-in-out" radius="full" size="sm"  variant="bordered"   >
+        <p className="font-bold text-blue-500">${price}</p>
+        <Button  className="bg-transparent hover:bg-slate-800 hover:text-white" radius="full" size="sm" variant="bordered"  onPress={() => router.push(`/services/${_id}`)} >
           Book Now
         </Button>
       </CardFooter>
@@ -33,4 +43,3 @@ const CarServiceCard = ({service}:{service:TService}) => {
 };
 
 export default CarServiceCard;
-
---
+++ src/services/singleCarService/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import envConfig from "@/src/config/envConfig"
+const getSingleCarService=async(id:string)=>{
+    const res=await fetch(`${envConfig.baseApi}/services/${id}`)
+
+    return res.json()
+}
+
+export default getSingleCarService
