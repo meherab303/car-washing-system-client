d638f574e4ae39333e255e95722cdae460756490
meherab nahin <nahinrahman87@gmail.com>
2025-03-12 16:50:33 +0600

    modify available slots

--- src/components/modules/service/AvailableSlots.tsx
+++ src/components/modules/service/AvailableSlots.tsx
@@ -39,28 +39,27 @@ const AvailableSlots = (serviceId:{serviceId:string}) => {
 
 
     return (
-
-     <div className="mt-2">
-            <Select
-         className="max-w-xs"
-    
-         
-        //  label=""
-         placeholder="available slots"
-
-         value={selectedSlot || ""}
-         onChange={(e) => setSelectedSlot(e.target.value)}
-         
-       >
-         {slots.map((slot:SlotBooking) => (
-           <SelectItem key={slot._id} value={slot.startTime}  > 
-           {slot.startTime}
-         </SelectItem>
-        
-         
-         ))}
-       </Select>
-     </div>
+<div className="mt-2">
+      <h3 className="text-lg font-semibold mb-2">Available Slots</h3>
+      <div className="grid grid-cols-3 gap-3">
+        {slots.length > 0 ? (
+          slots.map((slot) => (
+            <div
+              key={slot._id}
+              className="relative px-2 py-2 rounded-lg text-xs font-medium text-center shadow-sm 
+overflow-hidden bg-default-200 transition-all duration-700 ease-in-out
+before:absolute before:inset-0 before:bg-gradient-to-r before:from-blue-700 before:to-purple-700 
+before:opacity-0 hover:before:opacity-100 before:transition-opacity before:duration-700 before:-z-10 
+z-10"
+            >
+              {slot.startTime} to {slot.endTime}
+            </div>
+          ))
+        ) : (
+          <div className="text-gray-500 col-span-3 ">No slots available</div>
+        )}
+      </div>
+    </div>
     );
 };
 
